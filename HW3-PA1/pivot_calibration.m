b3204ec438d2c4947db86c58c196203cde12f875
emanmz <emanzaheer@utexas.edu>
2026-04-04 17:47:06 -0500

    pivot calibration

--- HW3-PA1/pivot_calibration.m
+++ HW3-PA1/pivot_calibration.m
@@ -1,19 +1,26 @@
 function [bTip, bPost] = pivot_calibration(R, p) %W9-L2 Slide 9-11
 % R = N x 3 rotational transformation between tracker and tool trackers
 % p = N x 1 translational transformation between tracker and tool trackers
-% we could also input F pointer and just extract the components...
 
-A = [];
-for %do some indexing here to call every 3x3 of R
-    Ai = Ri*eye(3);
-    A = [A; Ai]; %stack all the A matrices
+numFrames = size(R, 3);
+A = zeros(3 * numFrames, 6);
+B = zeros(3 * numFrames, 1);
+for i = 1:numFrames
+    Ri = R(:,:,i);
+    p_i = p(:,i);
+
+    % Row indices for current frame
+    rows = (3*i-2):(3*i);
+
+    A(rows, :) = [Ri, -eye(3)];
+    B(rows) = -p_i;
+
 end
+% least squares operator
+X = A \ B;
 
-B = -p;
-x = A/B;
 bTip = X(1:3,:);
 bPost = X(4:6,:);
 
-end 
+end
 
-%def need to check that this works but i dont know how
